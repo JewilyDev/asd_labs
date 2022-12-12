f15e7439ad155542b852e5ed1049e20f71710b38
jewily <jewilywork@gmail.com>
2022-12-12 15:33:47 +0200

    fixed lab1

--- lab1/cmake-build-debug/cmake_install.cmake
+++ lab1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jewil/CLionProjects/lab1
+# Install script for directory: C:/Users/jewil/CLionProjects/asd_labs/lab1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jewil/CLionProjects/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jewil/CLionProjects/asd_labs/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -5,17 +5,19 @@ int main() {
     std::string subseq_raw;
     std::cout << "Write down the subsequence" << std::endl;
     std::getline(std::cin,subseq_raw);
+    bool flag = false;
     for(int i = 0; i < subseq_raw.length(); i++){
         char bracket = subseq_raw[i];
         if(bracket == '(' || bracket == '{' || bracket == '['){
             subseq_stack.push(bracket);
+            flag = true;
         }else{
             int modifier;
             if(bracket == ')')
                 modifier = 1;
             else
                 modifier = 2;
-            if(subseq_stack.top() != char((int)bracket - modifier)){
+            if(subseq_stack.empty() || subseq_stack.top() != char((int)bracket - modifier)){
                 std::cout << "The subsequence is not exist" << std::endl;
                 bracket = 0;
                 break;
@@ -26,6 +28,6 @@ int main() {
         if(!bracket)
             break;
     }
-    if(subseq_stack.empty())
+    if(flag && subseq_stack.empty())
         std::cout << "The subsequence is exist" << std::endl;
 }
